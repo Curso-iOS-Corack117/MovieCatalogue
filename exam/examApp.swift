d3292830590a81430a2f58aec64deed23eac97a3
Ordaz Romero Sergio <orcheko@gmail.com>
2022-09-15 21:09:28 -0500

    Initial Commit

---
+++ exam/examApp.swift
@@ -0,0 +1,17 @@
+//
+//  examApp.swift
+//  exam
+//
+//  Created by Sergio Ordaz Romero on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct examApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
